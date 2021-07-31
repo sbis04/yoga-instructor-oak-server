0730480771d0063e779a251bdf83becc321a6a49
Souvik Biswas <sbis1999@gmail.com>
2021-07-31 23:53:50 +0530

    Update oak_starter

--- oak_starter.sh
+++ oak_starter.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# -----------------------------------------
+# Execute with:
+# ./oak_starter.sh <pose_name> <track_name>
+#
+# @example:
+# ./oak_starter.sh triangle1 beginners
+# -----------------------------------------
 
 # Navigate to the correct directory
-cd ~/yoga-instructor-oak-server/depthai_blazepose
+cd ~/yoga-instructor-oak-server/blazepose-oak
+
 # Run the script
-python3 -W ignore BlazeposeDepthai.py --pose & echo "PID: $!"
+python3 -W ignore BlazeposeDepthai.py --pose $1 --track $2 & echo "PID: $!"
